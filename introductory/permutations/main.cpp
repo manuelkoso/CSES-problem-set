64de8b560a420c7f49bf149cc6fd2ab248fa779c
Manuel Kosovel <kosovelmanuel@gmail.com>
2022-07-19 00:29:37 +0200

    Start Permutations

---
+++ introductory/permutations/main.cpp
@@ -0,0 +1,20 @@
+//
+// Created by koso on 19/07/22.
+//
+
+#include "iostream"
+
+void permutations(long long n) {
+
+
+
+}
+
+int main() {
+
+    long long n;
+    std::cin >> n;
+
+    permutations(n);
+
+}
