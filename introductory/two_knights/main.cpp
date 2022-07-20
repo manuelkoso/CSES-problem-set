a0a7371b532e6393ddb59a9b6f92a6bd48b374ca
Manuel Kosovel <kosovelmanuel@gmail.com>
2022-07-20 12:23:14 +0200

    Start Two Knights

---
+++ introductory/two_knights/main.cpp
@@ -0,0 +1,28 @@
+//
+// Created by koso on 20/07/22.
+//
+
+#include <array>
+#include "iostream"
+
+/*
+ * I think this problem can be done by backtracking.
+ * Skiena: Backtracking is a systematic way to run through all the possible configurations of a search space
+ */
+
+void two_knight(std::array<int,2> possible_solution) {
+    if() {
+
+    } else {
+
+    }
+}
+
+int main() {
+
+    int k;
+
+    std::cin >> k;
+
+    return 0;
+}
